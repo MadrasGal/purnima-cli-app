3fc543a7c176c494a6c7e750c490e54020133162
Purnima Lakshmi <codequeen1110@gmail.com>
2018-01-19 23:48:49 -0500

    added menu options

--- lib/pottermore_scraper/cli.rb
+++ lib/pottermore_scraper/cli.rb
@@ -3,7 +3,20 @@
 class PottermoreScraper::CLI
 
   def call 
-    puts ""Nitwit! Blubber! Oddment! Tweak! Welcome to Pottermore!"
+    puts "\n Nitwit! Blubber! Oddment! Tweak! Welcome to Pottermore!"
+    list_menu
   end
+
+  def list_menu
+    puts "\n"
+    puts "1. Search by name/character"
+    puts "2. Daily Prophet - News!"
+    puts "3. Sorting Hat"
+    puts "4. Diagon Alley - Shop!"
+    puts "\n"
+
+    input = gets.chomp 
+  end 
+
 
 end
