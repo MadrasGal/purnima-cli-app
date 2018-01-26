a7256f582a2cb8632dd22841aa7b1b6ee5386307
Purnima Lakshmi <codequeen1110@gmail.com>
2018-01-25 19:31:38 -0500

    testing objects

--- lib/pottermore_scraper/news.rb
+++ lib/pottermore_scraper/news.rb
@@ -1,5 +1,6 @@
 class PottermoreScraper::News
 
+  attr_accessor :title, :date, :author, :url 
 
   def self.items
 
@@ -7,6 +8,22 @@ class PottermoreScraper::News
 
     puts "HEllo World"
 
+    news1 = self.new
+    news1.title = "Title 1"
+    news1.date = "2018-01-01"
+    news1.author = "Pottermore Team"
+    news1.url = "https://www.pottermore.com/news/new-expanded-fantastic-beasts-and-where-to-find-them-vr-experience-announced"
+  
+
+    news2 = self.new
+    news2.title = "Title 2"
+    news2.date = "2018-01-01"
+    news2.author = "Pottermore Team"
+    news2.url = "https://www.pottermore.com/news/new-expanded-fantastic-beasts-and-where-to-find-them-vr-experience-announced"
+
+    [news1, news2]  
+
+    
   end
 
 
