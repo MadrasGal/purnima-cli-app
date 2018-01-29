64fae4a55bc1aa78428250663d088044de5f7f83
Purnima Lakshmi <codequeen1110@gmail.com>
2018-01-29 05:27:47 -0500

    updated scraper method to display all articles + added destroy_all method

--- lib/pottermore_scraper/news.rb
+++ lib/pottermore_scraper/news.rb
@@ -1,40 +1,79 @@
 require 'nokogiri'
 require 'open-uri'
 
-class PottermoreScraper::News
+class News
 
     attr_accessor :title, :date, :url 
 
-    def self.items #scrape pottermore.com and return news items 
-        #PottermoreScraper::NewsScraper.new("https://pottermore.com")
-        self.scrape_news
+    @@news = [] #stores all instances of this class with their attributes
+    
+    def self.items
+        self.destroy_all
+        self.scraper
+        
     end
 
-    #return instances of news items 
+    def self.scraper
+        
+        self.scrape_news
+        @@news
 
-    @news = []
-
-
-    def self.save_news(news_item)
-        @news << news_item 
-        @news
-    end 
+     end
 
     def self.scrape_news
 
-        #return array of news items 
+            doc = Nokogiri::HTML(open("https://www.pottermore.com/news"))
+             
+            news_item = doc.search("div.hub-item__content")
 
-        doc = Nokogiri::HTML(open("https://www.pottermore.com/news"))
+          
+            news_item.each do |news_doc|
+                
+                #div.hub-item__content.h2.hub-item__title
+                news_title = news_doc.search("h2.hub-item__title").text.strip
+                
+                news_date = news_doc.search("time.hub-item__date.News").text.strip
 
+                #element = news_doc.at_css('div#other-featured-items.news-hub__other-featured.hub-item a')
+                #news_url = element['href'] 
+                              
+                news = self.create_news_item(news_title, news_date)
 
-        news_item = self.new
-        news_item.title = doc.search("h2.hub-item--featured__title").text.strip
-        news_item.date = doc.search("time.hub-item--featured__date.News").text.strip
-    
-
-        element = doc.at_css('div.hub-item--featured a[href]')
-        news_item.url = element['href']               
-        
-        self.save_news(self)
+                news.save
+                news
+                      
+            end        
     end
+
+    def self.create_news_item(news_title, news_date)
+        news_item = self.new
+        news_item.title = news_title 
+        news_item.date = news_date  
+        #news_item.url = news_url 
+
+        news_item
+
+    end
+
+    def save  #saves instances to news array
+        @@news << self 
+    end 
+
+
+    def self.all #returns news array
+        @@news
+
+    end
+
+    def self.valid_number?(num)
+        num.to_i > 0 && num.to_i <= News.all.size
+    end
+
+    def self.find_by_id(index)
+        self.all[index.to_i-1]
+    end
+
+    def self.destroy_all
+        @@news.clear
+    end 
 end 
