a80429ecd25a6a27924f6e7faa2b0f8a4785a977
Purnima Lakshmi <codequeen1110@gmail.com>
2018-01-29 00:41:24 -0500

    updated menu options

--- lib/pottermore_scraper/cli.rb
+++ lib/pottermore_scraper/cli.rb
@@ -3,57 +3,70 @@
 class PottermoreScraper::CLI
 
   def call 
+    system("clear")
     puts "\n Nitwit! Blubber! Oddment! Tweak! Welcome to Pottermore!"
     list_menu
     user_input  
   end
 
-  #list menu options"
-
   def list_menu
     puts "\nMain Menu \n"
     puts "1. Daily Prophet - News!"
-    #puts "2. Diagon Alley - Shop!"
-    puts "2. Evanesco - Exit"
-    puts "\n"
-
-    @news = PottermoreScraper::News.items 
-
-    #display list of news items 
-
-    @news.each.with_index(1) do|news_item,index|
-     puts "#{index}. #{news_item.title} - #{news_item.date}"
-    binding.pry
-    end
-
+    puts "2. Evanesco - Exit\n"
   end
   
-  #gets user_input to proceed
 
   def user_input  
-
-    # to_do refine menu options
-    # insert check for integer 
-    # option to view menu again
-    # exit message 
     input = 'nil'
     while input != 'exit'
-      puts "To know more, enter 1 for option 1 or 2 to Exit'\n"
+      puts "\nTo see more, enter 'accio' or '1' for news, or '2' to Exit:\n"
       input = gets.strip.downcase
 
         case input
-            when "accio", "dailyprophet", "1"
-              puts "option 1"
-              the_ news_item = @news[input.to_i-1]
-              puts "#{index}. #{news_item.title} - #{news_item.date}"
-
+            when "accio", "dailyprophet", "1"  
+              display_news
+    
             when "evanesco","exit","2"
-              return 
+              puts "\nPip Pip, goodbye!"
+              puts "\n"
+              exit 
             else
               puts "\n Didn't catch the non-wizard lingo. Try again. \n"
-                 
+              list_menu
           end 
     end #while loop end 
   end 
+
+
+  def display_news
+    puts "\n Daily Prophet News" 
+    @news = PottermoreScraper::News.items 
+    @news.each.with_index(1) do |news_item, index|
+      puts "\n#{index}. #{news_item.title} - #{news_item.date}"
+    end 
+    user_input_news
+  end
+
+  def user_input_news
+
+      #ask user for which news item they would like to view in more detail
+      input = ' '
+
+      puts "Enter the number of the article to read or go back to the main menu."
+      input = gets.strip
+
+      if News.valid_number?(input)
+        news = News.find_by_id(input)
+
+        puts "Title: #{news.title}"
+        puts "Date: #{news.date}"
+        puts "Body: #{news.body}"
+
+        display_news
+      else
+        list_menu
+      end
+
+    end 
 
 end #end class
