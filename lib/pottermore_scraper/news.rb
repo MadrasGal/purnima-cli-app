75ed53f7ab0d366b1183d2025cea95883d82fdf2
Purnima Lakshmi <codequeen1110@gmail.com>
2018-01-29 18:07:16 -0500

    created separate scraper class and updated code

--- lib/pottermore_scraper/news.rb
+++ lib/pottermore_scraper/news.rb
@@ -12,65 +12,30 @@ class News
     end
 
     def self.scraper
-        
-        self.scrape_news
+        News_scraper.scrape_news
         @@news
 
      end
 
-    def self.scrape_news
-
-            doc = Nokogiri::HTML(open("https://www.pottermore.com/news"))
-             
-            news_item = doc.search(".hub-item")
-
-          
-            news_item.take(10).each do |news_doc|
-                
-                news_title = news_doc.search("h2.hub-item__title").text.strip
-                
-                news_date = news_doc.search("time.hub-item__date.News").text.strip
-
-                news_url = news_doc.search('a[href]').first.attributes['href'].value
-                # save instance           
-                news = self.create_news_item(news_title, news_date, news_url)
-                news.save
-                news
-                     
-            end        
-    end
-
-    def self.scrape_content(news_url)
-
-        #scraping for content 
-        full_url = URI.join('https://www.pottermore.com', news_url).to_s
-        
-        doc = Nokogiri::HTML(open(full_url))
-                
-        news_content = doc.search("div#news-body.news-story__content p").inner_text
-                 
-        news_content  
-
-    end
-
-    def self.create_news_item(news_title, news_date, news_url, news_content=nil)
+    
+    def self.create_news_item(news_title, news_date, news_url, news_content)
         news_item = self.new
         news_item.title = news_title 
         news_item.date = news_date  
         news_item.url = news_url 
-        
-        news_item.content = self.scrape_content(news_item.url)
+        news_item.content = news_content
 
+        #Instance saved in @@news
+        news_item.save
         news_item
-
     end
 
-    def save  #saves instances to news array
+    def save  
         @@news << self 
     end 
 
 
-    def self.all #returns news array
+    def self.all 
         @@news
 
     end
