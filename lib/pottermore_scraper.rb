2ebda1be9e0b38897708719ab914cb059cb44330
Purnima Lakshmi <codequeen1110@gmail.com>
2018-01-29 16:32:04 -0500

    updated required files

--- lib/pottermore_scraper.rb
+++ lib/pottermore_scraper.rb
@@ -4,6 +4,8 @@ require_relative './pottermore_scraper/cli'
 
 require_relative './pottermore_scraper/news'
 
+require_relative './pottermore_scraper/news_scraper.rb'
+
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
